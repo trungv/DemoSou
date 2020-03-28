882cd7cb9cfb0951ec47ac535ca81d628d0b2930
TrungV <vindvinn@gmail.com>
2020-03-29 00:25:21 +0700

    Try google doc

---
+++ GoogleDoc/Program.cs
@@ -0,0 +1,56 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Docs.v1;
+using Google.Apis.Docs.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace GoogleDoc
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/docs.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { DocsService.Scope.DocumentsReadonly };
+        static string ApplicationName = "Google Docs API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream(@"D:\Projects\GitLocal\DemoSou\GoogleDoc\credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Docs API service.
+            var service = new DocsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String documentId = "1EXSMM1qY56nfVWrMjp0-KVLmz0u6BGyufepRQEgkO-Y";
+            DocumentsResource.GetRequest request = service.Documents.Get(documentId);
+
+            // Prints the title of the requested doc:
+            // https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+            Document doc = request.Execute();
+            Console.WriteLine("The title of the doc is: {0}", doc.Title);
+        }
+    }
+
+}
